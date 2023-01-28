7c7e2f9e5e8da6e8fd45815a9b7d1a88247e5ab2
Mishu Dhar Chando <53133093+Chando0185@users.noreply.github.com>
2023-01-28 20:24:10 +0600

    Add files via upload

---
+++ code.ino
@@ -0,0 +1,43 @@
+const int pingPin = 7; // Trigger Pin of Ultrasonic Sensor
+const int echoPin = 6; // Echo Pin of Ultrasonic Sensor
+const int buzzer  =8;
+void setup() {
+   Serial.begin(9600); // Starting Serial Terminal
+}
+
+void loop() {
+   long duration, inches, cm;
+   pinMode(pingPin, OUTPUT);
+   digitalWrite(pingPin, LOW);
+   delayMicroseconds(2);
+   digitalWrite(pingPin, HIGH);
+   delayMicroseconds(10);
+   digitalWrite(pingPin, LOW);
+   pinMode(echoPin, INPUT);
+   duration = pulseIn(echoPin, HIGH);
+   inches = microsecondsToInches(duration);
+   cm = microsecondsToCentimeters(duration);
+   if(cm<12){
+    digitalWrite(buzzer, HIGH);
+   }
+   else{
+    digitalWrite(buzzer, LOW);
+   }
+   Serial.print(inches);
+  Serial.print("in, ");
+   Serial.print(cm);
+   Serial.print("cm");
+   Serial.println();
+   delay(100);
+}
+
+long microsecondsToInches(long microseconds) {
+   return microseconds / 74 / 2;
+}
+
+long microsecondsToCentimeters(long microseconds) {
+   return microseconds / 29 / 2;
+}
+
+
+
